28daef93e65a028e4bc20efa03447699e0649758
kollu <venkatkollu23@gmail.com>
2024-10-24 13:06:57 +0530

    testing sonar

---
+++ src/main/java/com/snkit/k8s/springbootdemo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.snkit.k8s.springbootdemo;
+
+public class HelloWorld {
+
+    public void test() {
+
+        try {
+            int i =10;
+
+
+        }catch (Exception e) {
+
+        }
+    }
+}
