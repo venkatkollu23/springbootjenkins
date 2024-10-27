70a6ace070abe1dae27ee583b34e21457c68c360
venkatkollu23 <155049106+venkatkollu23@users.noreply.github.com>
2024-10-27 13:12:59 +0530

    Update HelloWorld.java

--- src/main/java/com/neo/workshop/HelloWorld.java
+++ src/main/java/com/neo/workshop/HelloWorld.java
@@ -2,6 +2,6 @@ package com.neo.workshop;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!  test");
+        System.out.println("Hello World!  test  test");
     }
 }
